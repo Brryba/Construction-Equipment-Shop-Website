8156b4d6339ad10e79c19cfd7b2501637f92ae44
Brryba <jurabrezgunov@gmail.com>
2024-12-11 01:58:29 +0300

    j

---
+++ images/script.js
@@ -0,0 +1,18 @@
+//фиксирует меню сверху при прокрутке страницы, скрывает верхний баннер
+function pinMenu() {
+    function updateMenu()  {
+        const head_menu = document.getElementsByClassName("head-menu")[0];
+        const head_banner = document.getElementsByClassName("head-banner")[0];
+        if (window.scrollY >= head_banner.offsetHeight) {
+            head_menu.classList.add("fixed");
+            document.body.style.marginTop = `${head_menu.offsetHeight}px`;
+        } else {
+            head_menu.classList.remove("fixed");
+            document.body.style.marginTop = "0px";
+        }
+    }
+    window.addEventListener("scroll", updateMenu);
+    updateMenu();
+}
+
+document.addEventListener('DOMContentLoaded', pinMenu)
---
+++ scripts/create-header.js
@@ -0,0 +1,59 @@
+
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <div class="square"></div>
+            <div class="head-banner">
+                <a class="head-image" href="../index.html">
+                    <img src="images/head.png" alt="Логотип компании"/>
+                </a>
+                <div class="phone">+375 (17) 272-27-39</div>
+                <div class="email">info@mirostat.by</div>
+                <a class="address" href="https://yandex.by/maps/-/CHEIE05j" target="blank">Минск, ул.Радиальная,
+                    11Б/1,
+                    помещение №4</a>
+            </div>
+            <nav class="head-menu">
+                <ul>
+                    <li><a href="index.html">Главная</a></li>
+                    <li><a class="head-menu-caret" href="index.html">Новое оборудование</a>
+                        <ul>
+                            <li><a href="index.html">Пневмонагнетатели</a></li>
+                            <li><a>Подъёмники</a></li>
+                            <li><a>Компрессоры</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="head-menu-caret" href="bu.html">Техника Б/У</a>
+                        <ul>
+                            <li><a href="index.html">Пневмонагнетатели</a></li>
+                            <li><a>Подъёмники</a></li>
+                            <li><a>Компрессоры</a></li>
+                            <li><a>Прочее оборудование</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="head-menu-caret" href="arenda.html">Аренда техники</a>
+                        <ul>
+                            <li><a href="index.html">Пневмонагнетатели</a></li>
+                            <li><a>Подъёмники</a></li>
+                            <li><a>Компрессоры</a></li>
+                            <li><a>Пескоструйное оборудование</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="buy.html">Полезная информация</a></li>
+                    <li><a href="contact.html">Контакт</a></li>
+                </ul>
+            </nav>
+        </header>
+        `;
+    }
+}
+
+customElements.define("header-component", Header);
+
+const header = document.createElement("header-component");
+
+document.body.insertBefore(header, document.body.firstChild);
