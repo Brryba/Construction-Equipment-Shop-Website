33ab3e71426ffa7b5a0cf16ae8d9019c683625c2
Brryba <jurabrezgunov@gmail.com>
2024-12-15 14:45:56 +0300

    modal window updated

--- components/product-card.jsx
+++ components/product-card.jsx
@@ -12,6 +12,7 @@ function ProductCard(props) {
         setIsModalOpen(false);
         document.body.style.overflow = 'auto';
     };
+
     return (
         <div className="product-card-container">
             <h2>{props.name}</h2>
@@ -22,13 +23,25 @@ function ProductCard(props) {
                     <button onClick={handleOpenModal}>Подробнее</button>
                 </div>
                 <div className="photo">
-                    <img src={props.image} alt="Фото не найдено."></img>
+                    <img src={props.image} alt="Фото не найдено." onClick={handleOpenModal}></img>
                 </div>
             </div>
 
             {isModalOpen && (
-                <div className="modal" onClick={handleCloseModal}>
-                    <img src={props.image} alt={props.name} className="modal-image" />
+                <div className="modal-background" onClick={handleCloseModal}>
+                    <div className="modal" onClick={(closing) => closing.stopPropagation()}>
+                    <div className="images">
+                            <img src={props.image} alt={props.name} className="modal-image"/>
+                            {props.images}
+                        </div>
+                        <div className="content">
+                            <h2>{props.name}</h2>
+                            {props.features}
+                            {props.other}
+                            <h3>Цена: {props.price}</h3>
+                        </div>
+                        <span className="close-button" onClick={handleCloseModal}>Закрыть</span>
+                    </div>
                 </div>
             )}
         </div>
@@ -38,20 +51,37 @@ function ProductCard(props) {
 const productCards = document.querySelectorAll('.product-card');
 
 productCards.forEach(card => {
-    let temp = card.getAttribute('features');
-    let features = temp.split(';');
-    features.forEach((feature, index) => {
-        features[index] = feature.trim();
-    });
-    features = features.filter(item => item !== "");
-    const featuresList = features.map((item) => <p>{item}</p>);
+    function splitInput(notes) {
+        let notesArr = notes.split(';');
+        notesArr.forEach((note, index) => {
+            notesArr[index] = note.trim();
+        });
+        return notesArr.filter(item => item !== "");
+    }
+
+    function parseFeatures(features) {
+        if (features === undefined || features === null) {
+            return <div></div>;
+        }
+        let featuresArr = splitInput(features);
+        return featuresArr.length > 0 ? featuresArr.map((item) => <p>{item}</p>) : <div></div>;
+    }
+
+    function parseImages(images) {
+        if (images === undefined || images === null) {
+            return <div></div>;
+        }
+        let imagesArr = splitInput(images);
+        return imagesArr.length > 0 ? imagesArr.map((item) => <img src={item} alt="Фото не найдено."
+                                                                   className="modal-image"></img>) : <div></div>;
+    }
 
     ReactDOM.createRoot(card).render(<ProductCard
         name={card.getAttribute('name')}
-        features={featuresList}
+        features={parseFeatures(card.getAttribute('features'))}
         price={card.getAttribute('price')}
-        hidden={card.getAttribute('hidden')}
+        other={parseFeatures(card.getAttribute('other'))}
         image={card.getAttribute('image')}
-        images={card.getAttribute('images')}
+        images={parseImages(card.getAttribute('images'))}
     />);
 });
