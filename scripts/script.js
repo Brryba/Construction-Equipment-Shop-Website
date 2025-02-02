4afa5d18bd8ffbbbca11b4ee39c61d8f6307fb80
Brryba <jurabrezgunov@gmail.com>
2025-02-02 12:21:54 +0300

    mobile menu finished

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ function pinMenu() {
     function updateMenu()  {
         const head_menu = document.getElementsByClassName("head-menu")[0];
         const head_banner = document.getElementsByClassName("head-banner")[0];
-        const modal = document.getElementsByClassName("modal");
-        if (window.scrollY >= head_banner.offsetHeight) {
+        const modal = document.getElementsByClassName("head-banner");
+        if (window.scrollY >= head_banner.offsetHeight || window.scrollY >= modal.offsetHeight) {
             head_menu.classList.add("fixed");
             document.body.style.marginTop = `${head_menu.offsetHeight}px`;
         } else {
@@ -20,13 +20,14 @@ function mobileMenuOpenClose() {
     const button = document.getElementsByClassName("menu-opener")[0];
     const headMenu = document.getElementsByClassName('head-menu')[0];
     button.addEventListener('click', () => {
-        if (window.getComputedStyle(headMenu).display === "none") {
-            headMenu.style.display = "block";
+        if (window.getComputedStyle(headMenu).visibility === "hidden") {
+            headMenu.style.visibility = "visible";
             document.body.style.overflow = 'hidden';
         } else {
-            headMenu.style.display = "none";
+            headMenu.style.visibility = "hidden";
             document.body.style.overflow = '';
         }
+        headMenu.classList.toggle('opened');
     })
 }
 
